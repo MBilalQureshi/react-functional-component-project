019220c3634b371275a38834968438fad30d966b
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2023-12-23 18:06:17 +0000

    Implement basic of functional component

---
+++ my-app/src/components/FunctionalGreeting.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+// Option 1: Use regular function if complex
+// function FunctionalGreeting(){
+//     return <h1>Hello from React!</h1>
+// }
+
+// Option 2: Use aerow function if not complex(ES6)
+const FunctionalGreeting = () => <h1>Hello from React!!!</h1>
+
+export default FunctionalGreeting;
